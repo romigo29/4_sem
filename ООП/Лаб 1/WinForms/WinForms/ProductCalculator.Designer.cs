37971c2181b3e1f82a9265724a5da14e43e278ca
Igor Romanov <pigeon99905@gmail.com>
2025-02-14 17:20:17 +0300

    first

---
+++ ООП/Лаб 1/WinForms/WinForms/ProductCalculator.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WinForms
+{
+	partial class ProductCalculator
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProductCalculator));
+			this.Header = new System.Windows.Forms.Label();
+			this.Output = new System.Windows.Forms.RichTextBox();
+			this.ValuePerWeight = new System.Windows.Forms.Button();
+			this.CostPrice = new System.Windows.Forms.Button();
+			this.MonthUsages = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.Goods = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Amount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.DailyUsage = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Add = new System.Windows.Forms.Button();
+			this.Clear = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// Header
+			// 
+			resources.ApplyResources(this.Header, "Header");
+			this.Header.Name = "Header";
+			// 
+			// Output
+			// 
+			resources.ApplyResources(this.Output, "Output");
+			this.Output.Name = "Output";
+			// 
+			// ValuePerWeight
+			// 
+			resources.ApplyResources(this.ValuePerWeight, "ValuePerWeight");
+			this.ValuePerWeight.Name = "ValuePerWeight";
+			this.ValuePerWeight.UseVisualStyleBackColor = true;
+			this.ValuePerWeight.Click += new System.EventHandler(this.buttonValuePerWeight_Click);
+			// 
+			// CostPrice
+			// 
+			resources.ApplyResources(this.CostPrice, "CostPrice");
+			this.CostPrice.Name = "CostPrice";
+			this.CostPrice.UseVisualStyleBackColor = true;
+			this.CostPrice.Click += new System.EventHandler(this.buttonCostPrice_Click);
+			// 
+			// MonthUsages
+			// 
+			resources.ApplyResources(this.MonthUsages, "MonthUsages");
+			this.MonthUsages.Name = "MonthUsages";
+			this.MonthUsages.UseVisualStyleBackColor = true;
+			this.MonthUsages.Click += new System.EventHandler(this.ButtonMonthUsages_Click);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Goods,
+            this.Amount,
+            this.Value,
+            this.DailyUsage});
+			resources.ApplyResources(this.dataGridView1, "dataGridView1");
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowTemplate.Height = 24;
+			this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+			// 
+			// Goods
+			// 
+			resources.ApplyResources(this.Goods, "Goods");
+			this.Goods.Name = "Goods";
+			// 
+			// Amount
+			// 
+			resources.ApplyResources(this.Amount, "Amount");
+			this.Amount.Name = "Amount";
+			// 
+			// Value
+			// 
+			resources.ApplyResources(this.Value, "Value");
+			this.Value.Name = "Value";
+			// 
+			// DailyUsage
+			// 
+			resources.ApplyResources(this.DailyUsage, "DailyUsage");
+			this.DailyUsage.Name = "DailyUsage";
+			// 
+			// Add
+			// 
+			resources.ApplyResources(this.Add, "Add");
+			this.Add.Name = "Add";
+			this.Add.UseVisualStyleBackColor = true;
+			this.Add.Click += new System.EventHandler(this.buttonAdd_Click);
+			// 
+			// Clear
+			// 
+			resources.ApplyResources(this.Clear, "Clear");
+			this.Clear.Name = "Clear";
+			this.Clear.UseVisualStyleBackColor = true;
+			this.Clear.Click += new System.EventHandler(this.buttonClear_Click);
+			// 
+			// ProductCalculator
+			// 
+			resources.ApplyResources(this, "$this");
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.Clear);
+			this.Controls.Add(this.Add);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.MonthUsages);
+			this.Controls.Add(this.CostPrice);
+			this.Controls.Add(this.ValuePerWeight);
+			this.Controls.Add(this.Output);
+			this.Controls.Add(this.Header);
+			this.Name = "ProductCalculator";
+			this.Load += new System.EventHandler(this.ProductCalculator_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Label Header;
+		private System.Windows.Forms.RichTextBox Output;
+		private System.Windows.Forms.Button ValuePerWeight;
+		private System.Windows.Forms.Button CostPrice;
+		private System.Windows.Forms.Button MonthUsages;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button Add;
+		private System.Windows.Forms.Button Clear;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Goods;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Amount;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+		private System.Windows.Forms.DataGridViewTextBoxColumn DailyUsage;
+	}
+}
---
+++ ООП/Лаб 1/WinForms/WinForms/ProductCalculator.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms
+{
+	public partial class ProductCalculator : Form
+	{
+		List<Product> products = new List<Product>();
+
+		public class Product
+		{
+			public readonly double Markup = 15;
+			public string Name { get; set; }
+			public double Amount { get; set; }
+
+			public double Value { get; set; }
+
+			public double DailyUsage { get; set; }
+
+			public Product(string name, double amount, double value, double dailyUsage)
+			{
+				Name = name;
+				Amount = amount;
+				Value = value;
+				DailyUsage = dailyUsage;
+			}
+		}
+
+
+		public ProductCalculator()
+		{
+			InitializeComponent();
+		}
+
+		private void ProductCalculator_Load(object sender, EventArgs e)
+		{
+		
+		}
+
+		private void buttonAdd_Click(object sender, EventArgs e)
+		{
+
+			products.Clear();
+			
+			foreach( DataGridViewRow row in dataGridView1.Rows)
+			{
+				if (row.IsNewRow) continue;
+
+				string name = row.Cells[0].Value.ToString().Trim();
+				double amount = 0, value = 0, dailyUsage = 0;
+
+				try
+				{
+					if (string.IsNullOrEmpty(name))	throw new Exception("Название товара не может быть пустым");
+
+					if (!double.TryParse(row.Cells[1].Value.ToString(), out amount) || amount <= 0) throw new Exception("Введено некорректное значение количества товара");
+
+					if (!double.TryParse(row.Cells[2].Value.ToString(), out value) || value <= 0) throw new Exception("Введено некорректное значение стоимости товара");
+
+					if (!double.TryParse(row.Cells[3].Value.ToString(), out dailyUsage) || dailyUsage <= 0) throw new Exception("Введено некорректное значение ежеденевного потребления товара");
+
+				}
+
+				catch (Exception ex) 
+				{
+					MessageBox.Show($"Ошибка: {ex.Message}");
+				}
+
+				products.Add(new Product(name, amount, value, dailyUsage));
+			}
+
+		}
+
+		private void buttonClear_Click(object sender, EventArgs e)
+		{
+			dataGridView1.Rows.Clear();
+		}
+		private void buttonValuePerWeight_Click(object sender, EventArgs e)
+		{
+			Output.Clear();
+			string output = "Стоимость товар за килограмм: \n";
+
+			foreach (Product product in products)
+			{
+			
+				output += $"{product.Name} \t - \t";
+				output += $"{(product.Value * 1000) / product.Amount}р\n";
+			}
+
+			this.Output.Text = output;
+		}
+
+		private void buttonCostPrice_Click(object sender, EventArgs e)
+		{
+			Output.Clear();
+
+			string output = "Себестоимость товаров: \n";
+
+			foreach (Product product in products)
+			{
+
+				output += $"{product.Name} \t - \t";
+				output += $"{product.Value / (1 + product.Markup / 100)}р\n";
+			}
+
+			this.Output.Text = output;
+
+		}
+
+		private void ButtonMonthUsages_Click(object sender, EventArgs e)
+		{
+			Output.Clear();
+
+			const double nMonths = 30;
+			string output = "Расходов товаров товаров: \n";
+
+			foreach (Product product in products)
+			{
+
+				output += $"{product.Name} \t - \t";
+				output += $"{((product.DailyUsage * nMonths) / product.Amount) * product.Value}р/месяц\n";
+			}
+
+			this.Output.Text = output;
+		}
+
+		private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+
+		}
+	}
+}
