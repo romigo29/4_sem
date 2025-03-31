3c7cd67f06bc0b7dd9c8bf475096bb6548d212e5
Igor Romanov <pigeon99905@gmail.com>
2025-03-31 11:32:55 +0300

    тпи 5 start

---
+++ ТПвИ/Лаб 5/ASPA005/ASPA005_2/PhotoExistFilter.cs
@@ -0,0 +1,23 @@
+﻿using DAL004;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System.IO;
+
+public class PhotoExistFilter : IEndpointFilter
+{
+	public static IRepository repository; // Репозиторий с базовым путем к файлам
+
+	public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+	{
+		var celebrity = context.GetArgument<Celebrity>(0);
+		var result = await next(context); // Выполняем основной запрос
+
+		string? fileName = Path.GetFileName(celebrity.PhotoPath);
+		if (!File.Exists(Path.Combine(repository.BasePath, fileName)))
+		{
+			context.HttpContext.Response.Headers.Append("X-Celebrity", $"Not Found={fileName}");
+		}
+
+		return result;
+	}
+}
---
+++ ТПвИ/Лаб 5/ASPA005/ASPA005_2/SurnameFilter.cs
@@ -0,0 +1,24 @@
+﻿using DAL004;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+public class SurnameFilter : IEndpointFilter
+{
+	public static IRepository repository; // Репозиторий для проверки дубликатов
+
+	public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+	{
+		var celebrity = context.GetArgument<Celebrity>(0);
+		if (string.IsNullOrWhiteSpace(celebrity.Surname) || celebrity.Surname.Length < 2)
+		{
+			return Results.BadRequest("Surname cannot be empty or too short");
+		}
+
+		if (repository.getCelebritiesBySurname(celebrity.Surname).Length > 0)
+		{
+			return Results.BadRequest("Surname already exists");
+		}
+
+		return await next(context);
+	}
+}
